38d81e3264f28c5d6796c56a0cf3af1b311a2a1b
Rebeca Deliria Hein <141414439+rebecahein@users.noreply.github.com>
2023-08-11 08:18:31 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(600, 600);
+background ("black");}
+
+
+function draw() {
+ 
+
+  stroke('orange');
+  fill('gold');
+
+
+ if(mouseIsPressed){
+ rect(mouseX,mouseY,50,30);
+ }
+
+}
